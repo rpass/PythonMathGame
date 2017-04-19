0062d5eb5eea6b58b7fb4d50c1a553ecab21e5b9
Robert Passmore <robertmpassmore@gmail.com>
2017-04-19 23:51:01 +0200

    Reformat - create a CommandLineInterface class to contain the display logic

--- game.py
+++ game.py
@@ -1,37 +1,58 @@
 import random
 
 
-def format_operand(operand):
-    padding_spaces = " " * (5 - len(str(operand)))
-    return padding_spaces + str(operand)
-
-
 class Game(object):
     def __init__(self):
         self.turns = 30
+        self.user_interface = self.CommandLineInterface()
 
     def play(self):
+        self.user_interface.display_welcome()
         turn_counter = 0
         while turn_counter < self.turns:
-            turn = self.Turn()
+            turn = self.Turn(self)
             turn.play_turn()
             turn_counter += 1
         print("Thank you for playing!")
 
     class Turn(object):
-        def __init__(self):
+        def __init__(self, this_game):
+            self.game = this_game
             operand_range_min = 1
             operand_range_max = 100
             self.operands = (random.randint(operand_range_min, operand_range_max),
                              random.randint(operand_range_min, operand_range_max))
-            self.question = "%s\n%s +\n ----" % (format_operand(self.operands[0]),
-                                                 format_operand(self.operands[1]))
             self.answer = self.operands[0] + self.operands[1]
-            self.answer_string = "%s" % format_operand(self.answer)
 
         def play_turn(self):
-            input(self.question)
-            print(self.answer_string + "\n")
+            ui = self.game.user_interface
+            ui.ask_question(self.operands)
+            ui.display_answer(self.answer)
+
+    class CommandLineInterface(object):
+        @staticmethod
+        def display_welcome():
+            print("/ Hello, welcome to the math game \\")
+            print("--------- Don't be fooled ---------")
+            print("~~~~~~~~~~~~ ! Begin ! ~~~~~~~~~~~~")
+            print()
+            input(" Ready? \n")
+
+        @staticmethod
+        def format_operand(operand):
+            padding_spaces = " " * (5 - len(str(operand)))
+            return padding_spaces + str(operand)
+
+        def prepare_question(self, operands):
+            return "%s\n%s +\n ----" % (self.format_operand(operands[0]),
+                                        self.format_operand(operands[1]))
+
+        def ask_question(self, operands):
+            question = self.prepare_question(operands)
+            input(question)
+
+        def display_answer(self, answer):
+            print("%s\n" % self.format_operand(answer))
 
 
 if __name__ == "__main__":
--- gameTest.py
+++ gameTest.py
@@ -1,24 +1,27 @@
 import unittest
 
-from game import Game, format_operand
+from game import Game
 
 
 class GameTest(unittest.TestCase):
     def test_turn_answer_is_the_sum_of_the_operands(self):
         game = Game()
-        turn = game.Turn()
+        turn = game.Turn(game)
         self.assertEqual(turn.answer, turn.operands[0] + turn.operands[1])
 
 
-class FormatTesting(unittest.TestCase):
-    def test_operand_formats_positive_integers_correctly(self):
+class CommandLineInterfaceTest(unittest.TestCase):
+    def test_operand_formatter_formats_positive_integers_correctly(self):
+        game = Game()
         operand = 30
-        self.assertEqual(format_operand(operand), "   30")
+        self.assertEqual(game.user_interface.format_operand(operand), "   30")
 
-    def test_operand_formats_negative_integers_correctly(self):
+    def test_operand_formatter_formats_negative_integers_correctly(self):
+        game = Game()
         operand = -70
-        self.assertEqual(format_operand(operand), "  -70")
+        self.assertEqual(game.user_interface.format_operand(operand), "  -70")
 
-    def test_operand_formats_larger_than_expected_numbers_correctly(self):
+    def test_operand_formatter_formats_larger_than_expected_numbers_correctly(self):
+        game = Game()
         operand = 1234567
-        self.assertEqual(format_operand(operand), str(operand))
+        self.assertEqual(game.user_interface.format_operand(operand), str(operand))
