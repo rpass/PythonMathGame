7e4197e741031bceca93ee4f98e79378605a8284
Robert Passmore <robertmpassmore@gmail.com>
2017-04-19 17:14:28 +0200

    Refactor - extract turn mechanics into a 'play_turn' method of the Turn object

--- game.py
+++ game.py
@@ -9,15 +9,20 @@ class Game(object):
         turn_counter = 0
         while turn_counter < self.turns:
             turn = self.Turn()
-            print(turn.question)
-            input()
-            print(turn.answer)
+            turn.play_turn()
+            turn_counter += 1
+        print("Thank you for playing!")
 
     class Turn(object):
         def __init__(self):
             self.operands = (random.randint(-100, 100), random.randint(-100, 100))
             self.question = "%d + %d = ?" % self.operands
             self.answer = "%d" % (self.operands[0] + self.operands[1])
+
+        def play_turn(self):
+            print(self.question)
+            input()
+            print(self.answer)
 
 
 if __name__ == "__main__":
