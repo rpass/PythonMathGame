99c85dbbddbb271fb7510ace4fbe9a63155476a1
Robert Passmore <robertmpassmore@gmail.com>
2017-04-29 08:43:50 +0200

    Refactor - create setUp method to remove duplicate setup - move simulate_turn method out of the GameTest class

--- MathGameTest.py
+++ MathGameTest.py
@@ -4,48 +4,31 @@ from MathGame import Game
 
 
 class GameTest(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+        self.turn = self.game.Turn(self.game)
+
     def test_turn_answer_is_the_sum_of_the_operands(self):
-        game = Game()
-        turn = game.Turn(game)
-        self.assertEqual(turn.answer, turn.operands[0] + turn.operands[1])
+        self.assertEqual(self.turn.answer, self.turn.operands[0] + self.turn.operands[1])
 
     def test_check_user_input_increments_game_score_for_correct_input(self):
-        game = Game()
-        turn = game.Turn(game)
-        turn.ui = MockUI(turn)
-        turn.play_turn()
-        self.assertEqual(game.user_score, 1)
+        self.turn.ui = MockUI(self.turn)
+        self.turn.play_turn()
+        self.assertEqual(self.game.user_score, 1)
 
     def test_check_user_input_handles_none_string_input_as_an_incorrect_answer(self):
-        game = Game()
-        turn = game.Turn(game)
-        user_answer = ''
-        turn.check_user_input(user_answer)
-        user_answer = 1
-        turn.check_user_input(user_answer)
-        user_answer = 'one'
-        turn.check_user_input(user_answer)
-        user_answer = '3.14'
-        turn.check_user_input(user_answer)
-        user_answer = '!@#$'
-        turn.check_user_input(user_answer)
-        # Unicode for sunglass face emoji
-        user_answer = '\u1F60E'
-        turn.check_user_input(user_answer)
-        self.assertEqual(game.user_score, 0)
+        user_answers = ['', 1, 'one', '3.14', '!@#$', '\u1F60E']
+        for user_answer in user_answers:
+            self.turn.check_user_input(user_answer)
+        self.assertEqual(self.game.user_score, 0)
 
     def test_5_correct_answers_increases_difficulty_level_by_one(self):
         game = Game()
 
         for i in range(5):
-            self.simulate_turn(MockUI, game)
+            simulate_turn(MockUI, game)
 
         self.assertEqual(game.difficulty_level, 1)
-
-    def simulate_turn(self, MockUI, game):
-        turn = game.Turn(game)
-        turn.ui = MockUI(turn)
-        turn.play_turn()
 
 
 class MockUI(object):
@@ -54,6 +37,12 @@ class MockUI(object):
 
     def ask_question(self, operands):
         return self.correct_answer
+
+
+def simulate_turn(mock_ui, game):
+    turn = game.Turn(game)
+    turn.ui = mock_ui(turn)
+    turn.play_turn()
 
 
 if __name__ == '__main__':
